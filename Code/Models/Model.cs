c580183354ba66a7a43b5e7f12e6e09c327478b3
beersonic <beersonic@gmail.com>
2019-07-06 09:00:43 +0700

    Init project, add Model.cs

---
+++ Code/Models/Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace WebApplication1.Models
+{
+
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string ContactEmail { get; set; }
+        public string MobileNo { get; set; }
+        public ICollection<Transaction> Transactions { get; set; }
+    }
+
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public DateTime TransactionDateTime { get; set; }
+        public double Amount { get; set; }
+        public string CurrencyCode { get; set; }
+        public string Status { get; set; }
+    }
+}
